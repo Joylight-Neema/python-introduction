2460bd5c5841a8abe003fa3b7a5436b1ea8a3a8b
Joyligh-Neema <neemajoylight@gmail.com>
2025-08-07 05:02:44 -0700

    python variables and arithmetic opperations

---
+++ index.py
@@ -0,0 +1,25 @@
+print("hello world")
+#string
+name="neema"
+print(name)
+#number
+age=19
+print(age)
+#list-ordered and changeable
+marks=[10,20,30,40]
+#tuple-ordered but unchangeable
+ages=(10,20,30,40)
+#dictionary-a key value pair
+student={
+    "name":"neema",
+    "age":19,
+    "course":"game development"
+}
+print(marks)
+print(ages)
+print(student)
+#string concatenation-joining two or more strings into one
+first_name="joy"
+last_name="ruby"
+full_name=first_name +" "+last_name
+print(full_name)
