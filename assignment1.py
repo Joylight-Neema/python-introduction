8b635818725c58a9183e6d4707616ef649cd4fcf
Joyligh-Neema <neemajoylight@gmail.com>
2025-08-10 14:18:32 -0700

    added assignment1 progrm

---
+++ assignment1.py
@@ -0,0 +1,12 @@
+#users name
+name=input ("what's your name?")
+#favourite color
+color=input("what's your favourite color?")
+message= "hi"  + name +  "wow! you mean your favourite color is"  +  color  
+#message
+print(message)
+
+
+
+
+           
